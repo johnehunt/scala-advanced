654be9b48f9b8609bea11a2bca655cbce0a70567
John Hunt <john.hunt10@gmail.com>
2020-06-11 15:51:45 +0100

    Tidying

--- 06-reflection/src/com/jjh/reflect/TypeTagExampleApp2.scala
+++ 06-reflection/src/com/jjh/reflect/TypeTagExampleApp2.scala
@@ -3,6 +3,7 @@ package com.jjh.reflect
 object TypeTagExampleApp2 extends App {
 
   import scala.reflect.runtime.universe._
+
   case class Info[T: TypeTag] (data: T) {
     val tpe = typeOf[T]
   }
