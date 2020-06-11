820dc3c8d0d40ba69488a05cdc202ffc197a8815
John Hunt <john.hunt10@gmail.com>
2020-06-11 15:49:24 +0100

    Tidying

--- 02-traits/src/com/jjh/stackable/BasicIntQueue.scala
+++ 02-traits/src/com/jjh/stackable/BasicIntQueue.scala
@@ -4,6 +4,7 @@ import scala.collection.mutable.ArrayBuffer
 
 class BasicIntQueue extends IntQueue {
   private val buf = new ArrayBuffer[Int]
-  def get() = buf.remove(0)
-  def put(x: Int) { buf += x }
+
+  def get(): Int = buf.remove(0)
+  def put(x: Int): Unit = buf += x
 }
